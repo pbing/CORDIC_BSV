f9715751df83b97ab35d7e33297600b1956f1d72
Bernd Beuster <bernd.beuster@gmail.com>
2024-02-10 13:29:08 +0100

    Use C++20 for new Verilator releases

--- sim/verilator/vectoring/build.sh
+++ sim/verilator/vectoring/build.sh
@@ -6,6 +6,7 @@ verilator --cc \
           -Wno-INITIALDLY \
           -y ../../../verilog \
           -y $BLUESPECDIR/Verilog \
+          -CFLAGS -std=c++20 \
           mkCORDIC_v_16.v \
           Request.cpp \
           Response.cpp \
