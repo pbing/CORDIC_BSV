0ed7bf1c99c07c4635aa863288cd107bd43b1d99
Bernd Beuster <bernd.beuster@gmail.com>
2024-02-18 15:45:43 +0100

    Use zsh syntax

--- sim/verilator/rotating/build.sh
+++ sim/verilator/rotating/build.sh
@@ -6,8 +6,8 @@ verilator --cc \
           -Wno-INITIALDLY \
           -y ../../../verilog \
           -y $BLUESPECDIR/Verilog \
-          -CFLAGS "-std=c++20 `gsl-config --cflags`" \
-          -LDFLAGS "`gsl-config --libs`" \
+          -CFLAGS "-std=c++20 $(gsl-config --cflags)" \
+          -LDFLAGS "$(gsl-config --libs)" \
           mkCORDIC_r_16.v \
           Request.cpp \
           Response.cpp \
